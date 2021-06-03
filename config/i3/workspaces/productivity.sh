36e39ccbc52edcfa9ea1d3c8c1dc74c9ed5ea76e
Gregers Poulsen <gp@airborneinstruments.dk>
2021-06-03 17:18:45 +0200

    Bugfix workspace config with new basepath

--- config/i3/workspaces/productivity.sh
+++ config/i3/workspaces/productivity.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 # Setup Productivity Workspace
 
-i3-msg "workspace 1; append_layout /static/config/i3/workspaces/productivity.json"
+i3-msg "workspace 1; append_layout ~/.config/i3/workspaces/productivity.json"
 asana &
 dynalist &
